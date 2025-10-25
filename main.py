d646476078315bbc56bbd04e076788e7a0bc68cc
Pedro Goulart <pedrogoulart555@gmail.com>
2025-10-25 01:56:19 -0300

    inserção recursiva funcional (com alguns poréns)

--- main.py
+++ main.py
@@ -24,52 +24,90 @@ def handle_search(methods=['POST', 'GET']):
 @dataclass
 class trie_node:
    
-    index: Optional[str]
+    index: list[str]
     branches: Dict[str, 'trie_node'] = field(default_factory=dict)
-    
+
+# retorna um nó vazio
 def trie():
-    return trie_node(None, dict())
+    return trie_node([], dict())
+
+
+def compute_common_prefix(w1:str, w2:str):
+    common_prefix = ""
+    for i in range(min(len(w1), len(w2))):
+        if w1[i] != w2[i]:
+            break
+        common_prefix += w1[i]
+
+    return common_prefix
+
+def has_common_prefix(w1: str, w2: str):
+    if compute_common_prefix(w1, w2):
+        return True
+    return False
 
 
 def trie_insert(root: trie_node, word: str, filename: str):
-    
-    # CASO BASE
-    if not word:
-        root.index = filename
-        return
-    for key, child_node in root.branches.items():
-        common_prefix = ""
-        for i in range(min(len(word), len(key))):
-            if word[i] != key[i]:
-                break
-            common_prefix += word[i]
-        if not common_prefix:
-            continue
+    for key, node in root.branches.items():
+        if word.startswith(key):
+            restof_word = word[len(key):]
+            return trie_insert(node, restof_word, filename)
+        else:
+            common_prefix = compute_common_prefix(key, word)
+            if common_prefix:
+                restof_key = key[len(common_prefix):]
+                restof_word = word[len(common_prefix):]
+                root.branches.pop(key)
+                # o prefixo nao possui indice a principio
+                # se ele está sendo criado é pq nao achei
+                # palavra igual antes
+                root.branches[common_prefix] = trie_node(
+                    [], {restof_key: node
+                })
+                return trie_insert(
+                    root.branches[common_prefix], restof_word, filename
+                )
         
-        # CASO 1: a palavra inteira cabe perfeitamente em um galho que ja existe
-        if common_prefix == key:
-            rest_of_word = word[len(key):]
-            trie_insert(child_node, rest_of_word, filename)
-            return 
-        
-        # CASO 2: a inserção precisa da divisao de um galho
-        else: 
-            root.branches.pop(key)
-            intermediate_node = trie_node(None, dict())
-            rest_of_key = key[len(common_prefix):]
-            intermediate_node.branches[rest_of_key] = child_node
-            rest_of_word = word[len(common_prefix):]
-            new_leaf_node = trie_node(filename, dict())
-            intermediate_node.branches[rest_of_word] = new_leaf_node
-            root.branches[common_prefix] = intermediate_node
-            return 
+    root.branches[word] = trie_node([filename], {})
+    return root
 
-    root.branches[word] = trie_node(filename, dict())
 
 # visualizaçao
 def print_trie(node: trie_node, prefix=""):
-    if node.index is not None:
+    if len(node.index) != 0:
         print(f"{prefix} -> (Arquivo: {node.index})")
     for key, child in node.branches.items():
         print(f"{prefix}[{key}]")
         print_trie(child, prefix + "  ")
+
+
+
+# TODO: adaptar testes para o caso onde o node é dividido
+t = trie()
+trie_insert(t, 'peter', 't1')
+assert t == trie_node(
+    index=[],
+    branches={'peter': trie_node(index=['t1'], branches={})}
+)
+trie_insert(t, 'dampf', 't2')
+assert t == trie_node(
+    index=[],
+    branches={
+        'peter': trie_node(index=['t1'], branches={}),
+        'dampf': trie_node(index=['t2'], branches={})
+    }
+)
+trie_insert(t, 'donau', 't3')
+trie_insert(t, 'donaudampfschiff', 't3')
+trie_insert(t, 'donaudampfschiffahrt', 't3')
+assert t == trie_node(
+    index=[],
+    branches={
+        'peter': trie_node(['t1'], {}),
+        'dampf': trie_node(['t2'], {}),
+        'donau': trie_node(['t3'], {
+                    'dampfschiff': trie_node(['t3'], {
+                        'ahrt': trie_node(['t3'], {})
+                        })
+            })
+        })
