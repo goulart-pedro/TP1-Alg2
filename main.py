0af12cae213925c16c5d1dcac2f889142e62c1b6
Pedro Goulart <pedrogoulart555@gmail.com>
2026-05-02 00:27:14 -0300

    Adicionado: - display de centro atual com remoção - busca por endereços reais (código duplicado -- revisar) - filtragem de pontos no mapa e ocultação do retângulo de busca

--- main.py
+++ main.py
@@ -1,49 +1,51 @@
 import csv
-from dash_extensions.javascript import assign
+
 from dash import Dash, html, dcc, callback, Output, Input, State, dash_table, no_update
 import dash_leaflet as dl
 import dash_leaflet.express as dlx
 from dash_extensions.enrich import DashProxy
+from KDTree import construir_KDTree
+
+from utils.load_data import load_coordinates, load_butecos
+
+from components.map import map as mapthing
+
+
+from geopy.geocoders import Nominatim
+
+def descobre_coordenadas(endereco):
+    geolocator = Nominatim(user_agent="tp_algoritmos_2")
+
+    local = geolocator.geocode(endereco + ", Belo Horizonte, MG, Brasil")
+    if local: 
+        print(f"Endereço encontrado: {local.address}")
+        return {'lat': local.latitude, 'lon': local.longitude, 'address': local.address}
+    else:
+        print("Endereço não encontrado.")
+        return None
 
 # coordenadas centrais de belo horizonte
 bhz = (-19.9102, -43.9266)
 
-# Carregar coordenadas do arquivo results37-128.txt
-points = []
-with open('coordinates.txt', 'r', encoding='utf-8') as sample_file:
-    for line in sample_file:
-        line = line.strip()
-        if not line or line == ',':
-            continue
-        
-        try:
-            partes = line.split(',')
-            lat = float(partes[0].strip())
-            lon = float(partes[1].strip())
-            
-            points.append({
-                'lat': lat, 
-                'lon': lon, 
-                'popupContent': f"Coordenada: {lat:.4f}, {lon:.4f}"
-            })
-        except (ValueError, IndexError):
-            continue
-
-# Carregar butecos do CSV
-butecos = []
-with open('butecos_bh.csv', 'r', encoding='utf-8') as csv_file:
-    reader = csv.DictReader(csv_file, delimiter=';')
-    for i, row in enumerate(reader):
-        butecos.append({
-            'name': row['name'].strip(),
-            'address': row['address'].strip(),
-            'lat': points[i]['lat'],
-            'lon': points[i]['lon']
-        })
-
+points = load_coordinates('coordinates.txt')
+butecos = load_butecos(points, 'butecos_bh.csv')
 print('Indice Carregado')
+
+
 # Criar geojson para o mapa
 geojson_data = dlx.dicts_to_geojson(points)
+
+
+# Definir offset em graus (aproximadamente 0.01° = 1.1km)
+offset_lat = 0.1   # ~11km
+offset_lon = 0.1   # ~11km
+
+# Calcular bounds
+bounds = [
+    [bhz[0] - offset_lat, bhz[1] - offset_lon],  # sudoeste (canto inferior esquerdo)
+    [bhz[0] + offset_lat, bhz[1] + offset_lon]   # nordeste (canto superior direito)
+]
+
 
 app = DashProxy(__name__)
 
@@ -51,7 +53,8 @@ app.layout = html.Div([
     html.Div([
         # Componentes invisíveis para estado compartilhado
         dcc.Store(id='search-state', data=None),
-        html.H1("Mapa de Butecos - Belo Horizonte", className="main-title"),
+        dcc.Store(id='search-radius', data=2),
+        html.H1("Mapa da Breja - Belo Horizonte", className="main-title"),
         
         # Instruções
         html.Div([
@@ -71,37 +74,20 @@ app.layout = html.Div([
         
         # Barra de pesquisa
         html.Div([
-            html.Label("Buscar buteco na tabela:", className="search-label"),
             dcc.Input(
                 id='search-input',
                 type='text',
-                placeholder='Digite o nome do buteco...',
+                placeholder='Digite um endereço',
                 className="search-input"
             ),
-            html.Div(id='search-partials', className='search-partials')
+            dcc.Input(id='radius-input', type='number'),
+            dcc.Button(id='clear'),
+            # html.Div(id='search-partials', className='search-partials')
         ], className="search-container"),
         
         # Mapa
         html.Div([
-            dl.Map([
-                dl.TileLayer(),
-                dl.GeoJSON(
-                    id='map-geojson',
-                    data=geojson_data,
-                    cluster=True,
-                    zoomToBoundsOnClick=True,
-                    superClusterOptions={"radius": 100},
-                    options={
-                    'onEachFeature': assign("""
-                        function(feature, layer) {
-                            if (feature.properties && feature.properties.popupContent) {
-                                layer.bindPopup(feature.properties.popupContent);
-                            }
-                        }
-                    """)
-                    }
-                )
-            ],  id="map-container", center=bhz, zoom=11.5, style={'height': '500px', 'width': '100%'})
+            mapthing(geojson_data, None, bhz)
         ], className="map-container"),
         
         # Contadores
@@ -136,7 +122,7 @@ app.layout = html.Div([
     ], className="content-wrapper")
 ], className="main-container")
 
-
+# APENAS PARA TESTE
 def filter_butecos(butecos, search_value):
     search_lower = search_value.lower()
     
@@ -146,51 +132,64 @@ def filter_butecos(butecos, search_value):
     ]
 
 
-# Callback para os resultados parciais de busca
 @callback(
-    Output('search-partials', 'children'),
-    Input('search-input', 'value')
+    Output('search-radius', 'data'),
+    Input('radius-input', 'n_submit'),
+    State('radius-input', 'value')
 )
-def search_partials(search_value):
-    if not (search_value and search_value.strip()):
-        return []
-    
-    search_lower = search_value.lower()
-    filtered_butecos = filter_butecos(butecos, search_value)
+def update_search_radius(nsubmit, new_radius):
+    return new_radius
 
-    partial = lambda buteco: html.Div(className='partial-item', children=[
-        html.Span(buteco['name'], className='partial-name'),
-        html.Span(buteco['address'], className='partial-address')
-    ])
-    
-    return [partial(buteco) for buteco in filtered_butecos]
+
+@callback(
+    [Output('search-state', 'data', allow_duplicate=True),
+     Output('search-input', 'value', allow_duplicate=True)],
+    Input('clear', 'n_clicks'),
+    prevent_initial_call=True
+)
+def clear_selection(n_clicks):
+    if n_clicks:
+        return None, ''
+
+    return no_update, no_update
+
+
+@callback(
+    Output('clear', 'children'),
+    Input('search-state', 'data')
+)
+def update_address_display(center):
+    if center is None:
+        return 'Nada selecionado'
+
+    return center['address'] + ' ✕'
 
 
 # centralização do mapa após seleção de resultado
 @callback(
     Output('map-container', 'center'),
-    Output('search-input', 'value'),
     Output('search-state', 'data'),
     Input('search-input', 'n_submit'),
     State('search-input', 'value'),
     prevent_initial_call=True
 )
-def search_on_enter(n_submit, search_value):
+def update_map_center(n_submit, search_value):
+    print(f"Enviando busca {search_value}")
+    
     if n_submit and search_value and search_value.strip():
-        search_lower = search_value.lower()
-        filtered_butecos = filter_butecos(butecos, search_value)
+        fetched_center = descobre_coordenadas(search_value)
 
-        # se existem sugestões de busca, selecione a primeira e centralize-a
-        if filtered_butecos:
-            return (filtered_butecos[0]['lat'], filtered_butecos[0]['lon']), '', filtered_butecos[0]
+        # se existem sugestões de busca, selecione a primeira, centralize-a
+        # e guarde o selecionado em search-state
+        if fetched_center:
+            print('Novo centro: ', fetched_center['address'])
+            return fetched_center, fetched_center
 
         # se não, não modificar
         else:
-            return no_update, '', no_update
+            print("Nenhum resultado encontrado")
+            return no_update, no_update
 
-        # de qlq modo o campo de busca é limpo
-        # e consequentemente as sugestões somem
-    
     return bhz
 
 
@@ -201,7 +200,7 @@ def search_on_enter(n_submit, search_value):
     prevent_initial_call=True
 )
 def update_zoom(search_state):
-    return 16
+    return no_update
 
 
 # Callback para filtrar a tabela
@@ -215,6 +214,110 @@ def update_table(search_value):
         return filter_butecos(butecos, search_value)
 
     return butecos
+
+
+
+def pontos_relevantes (raiz_arvore, lat_min, lat_max, lon_min, lon_max, profundidade=0):
+    lista_pontos = []
+
+    if raiz_arvore is None:
+        return lista_pontos
+
+    lat_ponto = raiz_arvore.ponto[0]
+    lon_ponto = raiz_arvore.ponto[1]
+
+    # Verifica se a raiz está dentro dos parâmetros e a adiciona ou não na lista
+    if (lat_min <= lat_ponto <= lat_max) and (lon_min <= lon_ponto <= lon_max):
+        lista_pontos.append(raiz_arvore.ponto)
+
+    eixo = profundidade % 2
+
+    if eixo == 0: # Corte na Latitude
+        # Se o retângulo de busca tem qualquer parte à esquerda da linha de corte
+        if lat_min <= lat_ponto:
+            lista_pontos.extend(pontos_relevantes(raiz_arvore.esquerda, lat_min, lat_max, lon_min, lon_max, profundidade + 1))
+            
+        # Se o retângulo de busca tem qualquer parte à direita da linha de corte
+        if lat_max >= lat_ponto:
+            lista_pontos.extend(pontos_relevantes(raiz_arvore.direita, lat_min, lat_max, lon_min, lon_max, profundidade + 1))
+                
+    else: # Corte na Longitude
+        # Se o retângulo tem qualquer parte "abaixo" da linha de corte
+        if lon_min <= lon_ponto:
+            lista_pontos.extend(pontos_relevantes(raiz_arvore.esquerda, lat_min, lat_max, lon_min, lon_max, profundidade + 1))
+            
+        # Se o retângulo tem qualquer parte "acima" da linha de corte
+        if lon_max >= lon_ponto:
+            lista_pontos.extend(pontos_relevantes(raiz_arvore.direita, lat_min, lat_max, lon_min, lon_max, profundidade + 1))
+        
+
+    return lista_pontos
+
+
+
+@callback(
+    Output('map-geojson', 'data'),
+    Input('search-bounds', 'bounds'),
+    Input('search-state', 'data')
+)
+def update_points(bounds, search_state):
+    pontos_tupla = [(p['lat'], p['lon']) for p in points]
+    arvore_kdtree = construir_KDTree(pontos_tupla)
+    
+    print(f"Atualização com limites {bounds}")
+    
+    if bounds is None or search_state is None:
+        return geojson_data
+    
+    # bounds = [[lat_min, lon_min], [lat_max, lon_max]]
+    lat_min = bounds[0][0]
+    lon_min = bounds[0][1]
+    lat_max = bounds[1][0]
+    lon_max = bounds[1][1]
+    
+    pontos_filtrados = pontos_relevantes(
+        arvore_kdtree, 
+        lat_min, lat_max, 
+        lon_min, lon_max
+    )
+    
+    # Converter pontos filtrados para o formato original
+    pontos_dict = [
+        {'lat': p[0], 'lon': p[1], 'popupContent': f"Coordenada: {p[0]:.4f}, {p[1]:.4f}"}
+        for p in pontos_filtrados
+    ]
+    
+    return dlx.dicts_to_geojson(pontos_dict)
+
+
+@callback(
+    [Output('search-bounds', 'bounds'),
+     Output('search-bounds', 'pathOptions')],
+    [Input('search-state', 'data'),
+     Input('search-radius', 'data')],
+    prevent_initial_call=True
+)
+def update_rectangle(center, radius):
+    """Muda a cidad e geometria do r, ou o torna invisívelvisível) quando não há seleção"""
+    # Caso sem seleção
+    # estilo de rect só é mutável através de pathOptions no runtime
+    if not center:
+        print('Nenhum centro armazenado; Escondendo retângulo')
+        return no_update, {'opacity': 0, 'fillOpacity': 0}
+    
+    # Calcular bounds convertendo radius (km) para graus
+    # 1 grau ≈ 111.11 km
+    offset_lat = radius / 111.11
+    offset_lon = radius / 111.11
+    
+    bounds = [
+        [center['lat'] - offset_lat, center['lon'] - offset_lon],
+        [center['lat'] + offset_lat, center['lon'] + offset_lon]
+    ]
+    
+    print(f"Retângulo atualizado: {bounds}")
+    return bounds, {'opacity': 0.6, 'fillOpacity': 0.2}
+
 
 if __name__ == "__main__":
     app.run(debug=True)
